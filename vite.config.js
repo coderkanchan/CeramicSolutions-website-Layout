85d6e279ec57f3bded285db1ff9f4183a13b9f26
kanchan sharma <kanusharma881388@gmail.com>
2025-10-21 11:55:07 +0530

    fix: Final vite.config.js syntax and base path correction

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,10 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
+const repoName = 'CeramicSolutions-Website-Layout';
+
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss(),],
-  base: '/CeramicSolutions-website-Layout/'
+  plugins: [react()],
+  base: `/${repoName}/`
 })
