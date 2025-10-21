2608068843ec9f456c5b465d26e5bc8a5bb61fb4
kanchan sharma <kanusharma881388@gmail.com>
2025-10-21 12:03:14 +0530

    fix: Correct vite config syntax and resolve tailwind path error

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,10 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-const repoName = 'CeramicSolutions-Website-Layout';
+
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: `/${repoName}/`
+  base: '/CeramicSolutions-Website-Layout/'
 })
